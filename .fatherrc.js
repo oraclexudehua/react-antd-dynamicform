803ab9a4ebed9625a83093ac5e0e644ad7b93f3e
xdh <dehua.xu@mushiny.com>
2019-09-27 10:30:43 +0800

    Initial commit

---
+++ .fatherrc.js
@@ -0,0 +1,9 @@
+export default {
+  entry: 'src/index.js',
+  esm: {
+    type: 'babel',
+    importLibToEs: true,
+  },
+  cjs: 'babel',
+  extraBabelPlugins: [['import', { libraryName: 'antd', style: true }]],
+};
