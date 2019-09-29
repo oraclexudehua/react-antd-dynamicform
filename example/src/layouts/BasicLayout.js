cda02d604a2e69f450daa4e8995904badd69306f
xdh <dehua.xu@mushiny.com>
2019-09-29 09:34:33 +0800

    添加网络请求，编写例子

--- example/src/layouts/BasicLayout.js
+++ example/src/layouts/BasicLayout.js
@@ -1,14 +1,85 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Card } from 'antd';
+import { Card, Select, Input } from 'antd';
+import DynamicForm from '@/components/Dynamic'
+import axios from 'axios'
 
+const { DynamicFormCol } = DynamicForm
 export default class BasicLayout extends Component {
-  state = {};
+  state = {
+    list: []
+  };
 
+
+
+  componentDidMount() {
+    const _this = this
+    axios.get('http://127.0.0.1:3000/getList')
+      .then(function (response) {
+        // handle success
+        _this.setState({
+          list: response.data
+        })
+        // console.log(response);
+      })
+  }
+
+  renderOption = (record) => {
+    
+    return record.map(record => {
+      return (
+        <Select.Option value={record.age} >
+          {record.name}
+        </Select.Option>
+      )
+    })
+  }
   render() {
+    const { list } = this.state
+    debugger
     return (
       <div>
-        <Card />
+        <Card>
+          <DynamicForm>
+            <DynamicFormCol
+              field='name'
+              col={{ span: 10 }}>
+              <Select>
+                {this.renderOption(list||[])}
+                {/* <Select.Option value='WCS'>
+                  WCS
+                          </Select.Option>
+                <Select.Option value='WMS'>
+                  WMS
+                          </Select.Option> */}
+              </Select>
+            </DynamicFormCol>
+            <DynamicFormCol
+              field='value'
+              col={{ span: 10, offset: 1 }}
+              renderChild={(record) => {
+                if (record.name == 'WCS') {
+                  return (
+                    <Input />
+                  )
+                } else {
+                  return (
+                    <Select>
+                      <Select.Option value='WCS'>
+                        WCS
+                          </Select.Option>
+                      <Select.Option value='WMS'>
+                        WMS
+                          </Select.Option>
+                    </Select>
+                  )
+                }
+              }}
+            >
+
+            </DynamicFormCol>
+          </DynamicForm>
+        </Card>
       </div>
     );
   }
